9fd10ed29b54d06d06336a515ee8efe1b596793d
Azgrael <106307868+Azgrael@users.noreply.github.com>
2022-06-06 15:57:27 +0100

    begin

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height) //cria um retangulo com as propriedades X, Y, largura, altura (por esta ordem)
+
+const map = new Image()
+map.src = './img/O Mapa Labirinto Final.png'
+
+map.onload = () => {
+  c.drawImage(map, 0, 0)
+}
