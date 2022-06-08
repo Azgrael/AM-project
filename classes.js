d798319d7c3369f2a07928681b71e57329712534
RubenGualdino <84806814+RubenGualdino@users.noreply.github.com>
2022-06-08 22:18:38 +0100

    3

--- classes.js
+++ classes.js
@@ -3,38 +3,39 @@ class Sprite{
     constructor({position, velocity, image frames = { max: 1}}){
         this.position = position;
         this.image= image;
-        this.frames = frames;
+        this.frames = {...frames, val: 0, elapsed: 0 }
 
         this.image.onload = () => {
             this.with = this.image.with / this.frames.max
             this.heigth = this.image.heigth
         }
+       this.moving = false
     }
 
     draw() {
-            c.drawImage(
-                this.image,
-                0,
-                0,
-                this.image.width, / this.frames.max,
-                this.image.heigth,
-                this.position.x,
-                this.position.y,
-                this.image.with / this.frames.max,
-                this.image.heigth
+        c.drawImage(
+            this.image,
+            this.frames.val * this.width,
+            0,
+            this.image.width, / this.frames.max,
+            this.image.heigth,
+            this.position.x,
+            this.position.y,
+            this.image.with / this.frames.max,
+            this.image.heigth
         )
+
+        if (this.moving) {
+            if (this.frames.max > 1) {
+                this.frames.elapsed++
+            }
+            if (this.frames.elapsed % 10 == 0) {
+                if (this.frames.val < this.frames.max - 1) this.frames.val++
+                else this.frames.val++
+            }
+        }
     }
 }
 
-class Boundary {
-    static width = 32;
-    static heigth = 32;
-    constructor() {
-        this.position = position;
-        this.width = 32;
-        this.height = 32;
-    }
 
 
-
-}
