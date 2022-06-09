bc0dad398f6eae36db49a2f653950a2d169b00f7
RubenGualdino <84806814+RubenGualdino@users.noreply.github.com>
2022-06-09 16:14:56 +0100

    4

--- classes.js
+++ classes.js
@@ -1,43 +1,290 @@
-
-/* A personagem que temos tem 128x128 com 4 imagens em cada lateral, logo 128/4= 32*/
-
-class Sprite{
-    constructor({position, velocity, image, frames = { max: 1}}){
-        this.position = position;
-        this.image= image;
-        this.frames = {...frames, val: 0, elapsed: 0 }
-
+class Sprite {
+    constructor({
+                    position,
+                    velocity,
+                    image,
+                    frames = { max: 1, hold: 10 },
+                    sprites,
+                    animate = false,
+                    rotation = 0,
+                    scale = 1
+                }) {
+        this.position = position
+        this.image = new Image()
+        this.frames = { ...frames, val: 0, elapsed: 0 }
         this.image.onload = () => {
-            this.with = this.image.with / this.frames.max
-            this.heigth = this.image.heigth
+            this.width = (this.image.width / this.frames.max) * scale
+            this.height = this.image.height * scale
         }
-       this.moving = false
+        this.image.src = image.src
+
+        this.animate = animate
+        this.sprites = sprites
+        this.opacity = 1
+
+        this.rotation = rotation
+        this.scale = scale
     }
 
     draw() {
+        c.save()
+        c.translate(
+            this.position.x + this.width / 2,
+            this.position.y + this.height / 2
+        )
+        c.rotate(this.rotation)
+        c.translate(
+            -this.position.x - this.width / 2,
+            -this.position.y - this.height / 2
+        )
+        c.globalAlpha = this.opacity
+
+        const crop = {
+            position: {
+                x: this.frames.val * (this.width / this.scale),
+                y: 0
+            },
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+        }
+
+        const image = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+        }
+
         c.drawImage(
             this.image,
-            this.frames.val * this.width,
-            0,
-            this.image.width, / this.frames.max,
-            this.image.heigth,
-            this.position.x,
-            this.position.y,
-            this.image.with / this.frames.max,
-            this.image.heigth
+            crop.position.x,
+            crop.position.y,
+            crop.width,
+            crop.height,
+            image.position.x,
+            image.position.y,
+            image.width * this.scale,
+            image.height * this.scale
         )
 
-        if (this.moving) {
-            if (this.frames.max > 1) {
-                this.frames.elapsed++
-            }
-            if (this.frames.elapsed % 10 == 0) {
-                if (this.frames.val < this.frames.max - 1) this.frames.val++
-                else this.frames.val++
-            }
+        c.restore()
+
+        if (!this.animate) return
+
+        if (this.frames.max > 1) {
+            this.frames.elapsed++
+        }
+
+        if (this.frames.elapsed % this.frames.hold === 0) {
+            if (this.frames.val < this.frames.max - 1) this.frames.val++
+            else this.frames.val = 0
         }
     }
 }
 
+class Monster extends Sprite {
+    constructor({
+                    position,
+                    velocity,
+                    image,
+                    frames = { max: 1, hold: 10 },
+                    sprites,
+                    animate = false,
+                    rotation = 0,
+                    isEnemy = false,
+                    name,
+                    attacks
+                }) {
+        super({
+            position,
+            velocity,
+            image,
+            frames,
+            sprites,
+            animate,
+            rotation
+        })
+        this.health = 100
+        this.isEnemy = isEnemy
+        this.name = name
+        this.attacks = attacks
+    }
+
+    faint() {
+        document.querySelector('#dialogueBox').innerHTML = this.name + ' fainted!'
+        gsap.to(this.position, {
+            y: this.position.y + 20
+        })
+        gsap.to(this, {
+            opacity: 0
+        })
+        audio.battle.stop()
+        audio.victory.play()
+    }
+
+    attack({ attack, recipient, renderedSprites }) {
+        document.querySelector('#dialogueBox').style.display = 'block'
+        document.querySelector('#dialogueBox').innerHTML =
+            this.name + ' used ' + attack.name
+
+        let healthBar = '#enemyHealthBar'
+        if (this.isEnemy) healthBar = '#playerHealthBar'
+
+        let rotation = 1
+        if (this.isEnemy) rotation = -2.2
+
+        recipient.health -= attack.damage
+
+        switch (attack.name) {
+            case 'Fireball':
+                audio.initFireball.play()
+                const fireballImage = new Image()
+                fireballImage.src = './img/fireball.png'
+                const fireball = new Sprite({
+                    position: {
+                        x: this.position.x,
+                        y: this.position.y
+                    },
+                    image: fireballImage,
+                    frames: {
+                        max: 4,
+                        hold: 10
+                    },
+                    animate: true,
+                    rotation
+                })
+                renderedSprites.splice(1, 0, fireball)
+
+                gsap.to(fireball.position, {
+                    x: recipient.position.x,
+                    y: recipient.position.y,
+                    onComplete: () => {
+                        // Enemy actually gets hit
+                        audio.fireballHit.play()
+                        gsap.to(healthBar, {
+                            width: recipient.health + '%'
+                        })
+
+                        gsap.to(recipient.position, {
+                            x: recipient.position.x + 10,
+                            yoyo: true,
+                            repeat: 5,
+                            duration: 0.08
+                        })
+
+                        gsap.to(recipient, {
+                            opacity: 0,
+                            repeat: 5,
+                            yoyo: true,
+                            duration: 0.08
+                        })
+                        renderedSprites.splice(1, 1)
+                    }
+                })
+
+                break
+            case 'Tackle':
+                const tl = gsap.timeline()
+
+                let movementDistance = 20
+                if (this.isEnemy) movementDistance = -20
+
+                tl.to(this.position, {
+                    x: this.position.x - movementDistance
+                })
+                    .to(this.position, {
+                        x: this.position.x + movementDistance * 2,
+                        duration: 0.1,
+                        onComplete: () => {
+                            // Enemy actually gets hit
+                            audio.tackleHit.play()
+                            gsap.to(healthBar, {
+                                width: recipient.health + '%'
+                            })
+
+                            gsap.to(recipient.position, {
+                                x: recipient.position.x + 10,
+                                yoyo: true,
+                                repeat: 5,
+                                duration: 0.08
+                            })
+
+                            gsap.to(recipient, {
+                                opacity: 0,
+                                repeat: 5,
+                                yoyo: true,
+                                duration: 0.08
+                            })
+                        }
+                    })
+                    .to(this.position, {
+                        x: this.position.x
+                    })
+                break
+        }
+    }
+}
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        c.fillStyle = 'rgba(255, 0, 0, 0)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+/*
+
+Parte da pagina principal
 
 
+const image = new Image()
+image.src = './img/Pellet Town.png'
+
+const foregroundImage = new Image()
+foregroundImage.src = './img/foregroundObjects.png'
+
+const playerDownImage = new Image()
+playerDownImage.src = './img/playerDown.png'
+
+const playerUpImage = new Image()
+playerUpImage.src = './img/playerUp.png'
+
+const playerLeftImage = new Image()
+playerLeftImage.src = './img/playerLeft.png'
+
+const playerRightImage = new Image()
+playerRightImage.src = './img/playerRight.png'
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2
+  },
+  image: playerDownImage,
+  frames: {
+    max: 4,
+    hold: 10
+  },
+  sprites: {
+    up: playerUpImage,
+    left: playerLeftImage,
+    right: playerRightImage,
+    down: playerDownImage
+  }
+})
+*
+*
+*
+*
+*
+* */
